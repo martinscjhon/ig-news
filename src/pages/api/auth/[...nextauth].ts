a125d4fe24ce0806c27719096b1ac5de5b5fad9b
jhonatanMartins <martins.cjhon@gmail.com>
2022-11-11 15:37:20 -0300

    Conexao com faunadb e condicional para usuario duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna"
+import { query as q } from 'faunadb'
+
 export const authOptions = {
   providers: [
     GithubProvider({
@@ -7,5 +10,40 @@ export const authOptions = {
       clientSecret: `${process.env.CLIENT_GITHUB_SECRET}`,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }: any) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+
+    },
+  }
 }
+
 export default NextAuth(authOptions)
